2b8a5fb0c2fa618afb0282fa23da5876fb8a8a76
Cybaries <amankantjha@gmail.com>
2022-02-08 16:17:57 +0530

    Added types and aligned name&id

--- script.js
+++ script.js
@@ -12,26 +12,36 @@ const generateHtml = (data) => {
         let img = create('img')
         node.className = 'pokemon'
         img.className = 'images'
-        // details.className = 'details'
-        // details.id = '${poke.name}'
-        append(node,img)
+        name.innerHTML ='Name: '+poke.name.charAt(0).toUpperCase() + poke.name.substring(1)
+        
         fetch(poke.url)
             .then((dat) => dat.json())
             .then((pokemon) => {
                 let ID = create('span')
                 ID.className = 'id'
-                ID.innerHTML ='ID:'+pokemon.id
+                ID.innerHTML = 'Id:' + pokemon.id
+                let type = create('span')
+                type.className = 'type'
+                let m = typeset(pokemon.types) 
+                type.innerHTML = m
+                append(node,img)
                 append(node, ID)
-                
+                append(node, name)
+                append(node,type)
                 img.src = pokemon.sprites.front_default
             })
-        name.innerHTML ='Name: '+poke.name.charAt(0).toUpperCase() + poke.name.substring(1)
-        append(node, name)
-        // append(node, details)
-        myglobal = (pok) => {
-            
-        }
         document.querySelector('.list').appendChild(node)
+        typeset = (x) => {
+            // console.log(x)
+            let p = ''
+            for (let i = 0; i < x.length; i++) {
+                p += ` 
+                <div class = 'types'>${x[i].type.name.charAt(0).toUpperCase() + x[i].type.name.substring(1)}</div>
+                `
+            }
+            // console.log(p)
+            return p
+        }
     })
 }
 create = (val) => {
@@ -41,3 +51,15 @@ create = (val) => {
 append = (node, element) => {
     node.appendChild(element)
 }
+
+let t = document.querySelectorAll('.types')
+console.log(t)
+t.forEach(x => {
+    console.log(x)
+    switch (x.text) {
+        case 'Green': {
+            // x.style.background - color= 'green';
+            break;
+        }
+    }
+})
