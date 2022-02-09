c27312f980e7cb432179bed42ed4e5c245522da7
Cybaries <amankantjha@gmail.com>
2022-02-10 00:03:54 +0530

    Added Loader

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let bg = document.querySelector('body');
+let loader = document.querySelector('.loader');
+
+let load = 0;
+let int = setInterval(count, 0);
+
+function count() {
+    load++;
+    if (load > 99)
+        clearInterval(int);
+    bg.style.filter = `blur(${scale(load, 0, 40, 30, 0)}px)`;
+    loader.innerHTML = `${load}%`;
+    loader.style.opacity = scale(load, 0, 100, 100, 0);
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
+
+// API Call
+
 let apiurl = 'https://pokeapi.co/api/v2/pokemon?limit=898&offset=0'
 
 fetch(apiurl)
